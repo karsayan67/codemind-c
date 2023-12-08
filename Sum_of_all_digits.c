5b21ca0bb85bf5392d88e72dc8a723002e0556d1
karsayan67 <151984920+karsayan67@users.noreply.github.com>
2023-12-08 12:10:32 +0530

    #include<stdio.h> int main()     {         int x,sum=0;         scanf("%d",&x);         for(int i=x;i>0;i--)         {             int r=x%10;              sum=sum+r;             x=x/10;
            }
            printf("%d",sum);
        }

--- Sum_of_all_digits.c
+++ Sum_of_all_digits.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
-{
-    int x,sum=0;
-    scanf("%d",&x);
-    while(x>0)
     {
-        int r=x%10;
-        x=x/10;
-        sum=sum+r;
+        int x,sum=0;
+        scanf("%d",&x);
+        for(int i=x;i>0;i--)
+        {
+            int r=x%10;
+             sum=sum+r;
+            x=x/10;
+           
+        }
+        printf("%d",sum);
     }
-    printf("%d",sum);
-}
