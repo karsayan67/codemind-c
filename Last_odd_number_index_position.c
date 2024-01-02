e83e28b6c0275acc6a4d35e25322fc31404962e1
karsayan67 <151984920+karsayan67@users.noreply.github.com>
2024-01-02 21:54:44 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     int lastodd=-1;     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             lastodd=i;         }     }     if(lastodd!=-1)     {         printf("%d",lastodd);     } }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int lastodd=-1;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            lastodd=i;
+        }
+    }
+    if(lastodd!=-1)
+    {
+        printf("%d",lastodd);
+    }
+}
